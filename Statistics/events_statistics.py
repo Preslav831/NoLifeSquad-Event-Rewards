d3a27fdcbc682325c309a5dc72d923fed50e6d0e
Preslav Kolev <Kolevpreslav91@Gmail.com>
2025-02-18 20:33:45 +0200

    Added ideas for data analyzer.

---
+++ Statistics/events_statistics.py
@@ -0,0 +1,17 @@
+#Statistics for winners
+#statistics for most wanted event by staff
+#statistics for most wanted event by members
+#statistics for most played events
+#
+# class Winners:
+#     def __init__(self, first_place, second_place, third_place):
+#         self.first_place = first_place
+#         self.second_place = second_place
+#         self.third_place = third_place
+#
+#         self.first_place_lst = []
+#         self.second_place_lst = []
+#         self.third_place_Lst = []
+#
+#     def add_to_lst(self):
+#         self.first_place
